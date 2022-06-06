85b0af03f407dbade455159d556f9492beda354e
Krzysztof Matyjaszczyk <krzysiek.matyjaszczyk@gmail.com>
2022-06-06 13:15:57 +0200

    Create layout with elements

---
+++ app/src/main/java/com/example/lab5/DrawingSurface.java
@@ -0,0 +1,34 @@
+package com.example.lab5;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+public class DrawingSurface extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+    public DrawingSurface(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.lab5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawingSurface mDrawingSurface;
+    private Button mButtonColorRed;
+    private Button mButtonColorOrange;
+    private Button mButtonColorBlue;
+    private Button mButtonColorGreen;
+    private Button mButtonClear;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        connectFieldsWithLayoutElements();
+    }
+
+    private void connectFieldsWithLayoutElements() {
+        mDrawingSurface = findViewById(R.id.drawingSurface);
+        mButtonColorRed = findViewById(R.id.buttonColorRed);
+        mButtonColorOrange = findViewById(R.id.buttonColorOrange);
+        mButtonColorBlue = findViewById(R.id.buttonColorGreen);
+        mButtonClear = findViewById(R.id.buttonClear);
     }
 }
