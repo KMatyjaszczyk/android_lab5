5471b53b0767f350a62b9ccfd99902ca6ad37959
Krzysztof Matyjaszczyk <krzysiek.matyjaszczyk@gmail.com>
2022-06-12 10:45:53 +0200

    Implement basic image manipulation

--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -3,10 +3,11 @@ package com.example.lab5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private DrawingSurface mDrawingSurface;
+    private DrawView drawView;
     private Button mButtonColorRed;
     private Button mButtonColorOrange;
     private Button mButtonColorBlue;
@@ -20,10 +21,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         connectFieldsWithLayoutElements();
+        drawView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                drawView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                int height = drawView.getHeight();
+                int width = drawView.getWidth();
+                drawView.init(height, width);
+            }
+        });
     }
 
     private void connectFieldsWithLayoutElements() {
-        mDrawingSurface = findViewById(R.id.drawingSurface);
+        drawView = findViewById(R.id.drawingSurface);
         mButtonColorRed = findViewById(R.id.buttonColorRed);
         mButtonColorOrange = findViewById(R.id.buttonColorOrange);
         mButtonColorBlue = findViewById(R.id.buttonColorGreen);
